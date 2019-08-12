3b06856b6a26669a68f7df69585a72ff77a5963d
chaozi <1243352372@qq.com>
2019-08-12 15:30:32 +0800

    no message

--- app/Console/functions.php
+++ app/Console/functions.php
@@ -1,7 +1,26 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: root
- * Date: 2019/8/12
- * Time: 15:12
- */
+function randomString($length = 4, $type = 'string', $convert = 0){
+          $config = array(
+              'number' => '1234567890',
+              'letter' => 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+              'string' => 'abcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ23456789',
+              'all' => 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+        );
+
+       if (!isset($config[$type]))
+                   $type = 'string';
+
+         $string = $config[$type];
+
+        $code = '';
+        $strlen = strlen($string) - 1;
+        for ($i = 0; $i < $length; $i++) {
+            $code .= $string{mt_rand(0, $strlen)};
+        }
+
+        if (!empty($convert)) {
+              $code = ($convert > 0) ? strtoupper($code) : strtolower($code);
+        }
+
+         return $code;
+    }
