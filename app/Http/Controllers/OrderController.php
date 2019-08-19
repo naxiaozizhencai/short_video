9aba87a2b187a53d5229f951b71db9f9d8494827
guokaicheng <13262857937@163.com>
2019-08-19 21:18:21 +0800

    充值

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Service\OrderService;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class OrderController extends Controller
+{
+    protected $orderService;
+    public function __construct(OrderService $orderService)
+    {
+        $this->orderService = $orderService;
+    }
+
+    //充值产品列表
+    public function ProductList($value='')
+    {
+        $data = $this->orderService->GetProductList();
+        return response()->json($data);
+    }
+
+    /**
+     * 提交订单
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function CreateOrder(Request $request)
+    {
+        $user_id = Auth::user()->id;
+        
+        $data = $this->orderService->SaveOrder($user_id,$request);
+        return response()->json($data);
+
+    }
+    /**
+     * 充值明细
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function PayDetails(Request $request)
+    {
+        $user_id = Auth::user()->id;
+        $data = $this->orderService->GetOrderList($user_id);
+        return response()->json($data);
+
+    }
+}
---
+++ app/Service/OrderService.php
@@ -0,0 +1,70 @@
+<?php
+namespace App\Service;
+use App\Repositories\OrderRepositories;
+
+class OrderService
+{
+    protected $orderRepositories;
+    public function __construct(OrderRepositories $orderRepositories)
+    {
+        $this->orderRepositories = $orderRepositories;
+    }
+
+    /**
+     *获取产品信息
+     * @return array
+     */
+    public function GetProduct($product_id)
+    {
+        $result = $this->orderRepositories->getProductData($product_id);
+
+        if(empty($result)){
+            return $data = ['code'=>-1, 'errMsg'=>'数据不存在'];
+        }
+        return $result;
+    }
+
+     /**
+     *保存订单
+     * @return array
+     */
+    public function SaveOrder($user_id,$request)
+    {
+        $data =  ['code'=>-1, 'errMsg'=>'订单创建失败'];
+        $order_type = $request['order_type'];
+        $product_id = $request['product_id'];
+        $product = $this->GetProduct($request['product_id']);
+        $order_price = $product->product_price;
+        $pay_type = $request->input('pay_type');
+        $result = $this->orderRepositories->InsertUserOrder($user_id,$order_type,$product_id,$order_price,$pay_type);
+
+        if(!empty($result)){
+            return $data =['code'=>200, 'errMsg'=>'订单创建成功'];
+        }
+        return $data;
+    }
+
+    public function GetOrderList($user_id)
+    {
+        $result = $this->orderRepositories->getOrderData($user_id);
+
+        if(empty($result)){
+            return $data = ['code'=>-1, 'errMsg'=>'订单数据不存在'];
+        }
+        return $result;
+    }
+
+
+    public function GetProductList()
+    {
+        $result = $this->orderRepositories->getProductData($user_id);
+
+        if(empty($result)){
+            return $data = ['code'=>-1, 'errMsg'=>'数据不存在'];
+        }
+        return $result;
+    }
+
+
+
+}
