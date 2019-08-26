71a8fc9c7fc37de64dae29ed587d6c176d57953e
chaozi <1243352372@qq.com>
2019-08-26 16:06:42 +0800

    no message

--- app/Repositories/UsersRepositories.php
+++ app/Repositories/UsersRepositories.php
@@ -139,7 +139,7 @@ class UsersRepositories
     public function GetInvitationRankData()
     {
         return DB::table('users')->
-        leftJoin('users_detail', 'users.id', '=', 'users_detail.user_id')->where('users.detail.invitation_num', '>', 0)->
+        leftJoin('users_detail', 'users.id', '=', 'users_detail.user_id')->where('users_detail.invitation_num', '>', 0)->
         select(['users.*','users_detail.*'])->orderBy('invitation_num', 'desc')->limit(50)->get();
     }
 
