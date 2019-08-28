fbc68a58bef978e6765fb4ef7fcffaa198e17ae3
guokaicheng <13262857937@163.com>
2019-08-28 23:03:02 +0800

    bug

--- app/Http/Controllers/NotifyController.php
+++ app/Http/Controllers/NotifyController.php
@@ -18,16 +18,16 @@ class NotifyController extends Controller
     public function Notify(){
         //加载fastpay支付插件
 
-        if (!function_exists('get_openid')) {
-        require $_SERVER['DOCUMENT_ROOT'].'/fastpay/Fast_Cofig.php';
-        }
-
-        $sign=$_POST['sign_notify'];//获取签名2.07版,2.07以下请使用$sign=$_POST['sign'];
-        $check_sign=notify_sign($_POST);
-        if($sign!=$check_sign){
-          exit("签名失效");
-        //签名计算请查看怎么计算签名,或者下载我们的SDK查看
-        }
+//        if (!function_exists('get_openid')) {
+//        require $_SERVER['DOCUMENT_ROOT'].'/fastpay/Fast_Cofig.php';
+//        }
+//
+//        $sign=$_POST['sign_notify'];//获取签名2.07版,2.07以下请使用$sign=$_POST['sign'];
+//        $check_sign=notify_sign($_POST);
+//        if($sign!=$check_sign){
+//          exit("签名失效");
+//        //签名计算请查看怎么计算签名,或者下载我们的SDK查看
+//        }
 
         //更新数据库
         $data = $this->orderService->UpdateOrder($_POST);
