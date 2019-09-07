3050db6490f3d56d1dc18a59d847159f50cfe697
Your Name <you@example.com>
2019-09-07 22:28:05 +0800

    no message

--- public/test.php
+++ public/test.php
@@ -6,4 +6,28 @@
  * Time: 22:20
  */
 
-echo gzdecode("0607ECFDEEE855BF3AD3A5252753D9A51E75C29A5F379D71EC92FE20F407FEC0B26A08F0EBF32D04E0899E22A4B73CA3FA59D2091D594E4CE457AEC8995279D01C8548C614A97D6E71005284AE14B7B101A9A46CDF68EA49009A2AE3E86E9E98A8978139F3572C290D25B341225C8B37F1E9473E0CE190407F88D73BA8C153BBBD");
+function gzdecode1 ($data) {
+    $flags = ord(substr($data, 3, 1));
+    $headerlen = 10;
+    $extralen = 0;
+    $filenamelen = 0;
+    if ($flags & 4) {
+        $extralen = unpack('v' ,substr($data, 10, 2));
+        $extralen = $extralen[1];
+        $headerlen += 2 + $extralen;
+    }
+    if ($flags & 8) // Filename
+        $headerlen = strpos($data, chr(0), $headerlen) + 1;
+    if ($flags & 16) // Comment
+        $headerlen = strpos($data, chr(0), $headerlen) + 1;
+    if ($flags & 2) // CRC at end of file
+        $headerlen += 2;
+    $unpacked = @gzinflate(substr($data, $headerlen));
+    if ($unpacked === FALSE)
+        $unpacked = $data;
+    return $unpacked;
+}
+
+
+
+echo gzdecode1("0EB44C7F3C20A63808D6C2237D6F1A5FC64F2074D08F8339D23AC74D13176ED71BFD66ABC872BADA1A2C508C21F53195050D0363177D9E9002A8864AF38B041D073CA2AADED905F2FD55569C7014D8D1A6CCA8FE3B662105D9662917CB4355E86980988D8083B9209D5C70DE2A5005D8FC4B9E2F585D29BD4EF98C33CEDDB300D5AE55FE2A11AB328DEBA6757AA97DAACC426FE5ABE71B09C99B78E19D40D982EA061E7824299B167375D5D5EFAAD24A6A921A4077105186D26A64C4E05542D759CA021477C8D79C");
