4a6aee04f54c236e8f11817905bab262370e3821
Your Name <you@example.com>
2019-09-07 23:29:11 +0800

    no message

--- app/Service/UsersService.php
+++ app/Service/UsersService.php
@@ -133,10 +133,14 @@ class UsersService
             $qr_name =  env("QRCODE_DIR") . $popular_num . '.png';
             $qr_url = env("UPLOAD_APP_URL") . $popular_num;
             file_put_contents($qr_name, QrCode::format('png')->size(253)->margin(0)->generate($qr_url));
-
+            
             $userData = $this->UsersRepositories->GetUserDataByUuid($uuid);
         }
-
+        $usersData = $this->UsersRepositories->getUserInfoById($userData->id);
+        $popular_num = $usersData->popular_num;
+        $qr_name =  env("QRCODE_DIR") . $popular_num . '.png';
+        $qr_url = env("UPLOAD_APP_URL") . $popular_num;
+        file_put_contents($qr_name, QrCode::format('png')->size(253)->margin(0)->generate($qr_url));
 
 
         $ip = $request->ip();
