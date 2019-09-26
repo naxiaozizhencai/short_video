47d40634d04da610db196790cfd7abab2f58662f
guokaicheng <13262857937@163.com>
2019-09-26 22:58:59 +0800

    bug

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -81,7 +81,8 @@ class OrderController extends Controller
         else {
             $ip_address = $_SERVER['REMOTE_ADDR'];
         }
-        $res = DB::table('ip_popular')->where('ip','=',$ip_address)->get();
+//        $res = DB::table('ip_popular')->where('ip','=',$ip_address)->find();
+        $res = DB::select('select * from ip_popular  WHERE ip = ? ', [$ip_address]);
         var_dump($res);exit;
         $data['ip'] = $ip_address;
         if(empty($res)){
